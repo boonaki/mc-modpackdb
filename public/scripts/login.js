bb7b21d8db7f0b16433c3cc51190e2c35a75f084
Raymond Guo <45112078+RayGuo357@users.noreply.github.com>
2022-06-21 12:49:38 -0400

    Update login.js

--- public/scripts/login.js
+++ public/scripts/login.js
@@ -2,14 +2,19 @@ window.addEventListener("load", function () {
     const login = document.getElementById('login')
 
     login.addEventListener('submit', (event) => {
+        // Stops the default submit action and allows us to perform our own aciton
         event.preventDefault()
+        
+        // Saves the data from the form into FD
         let FD = new FormData(login)
 
+        // Creates the JSON data from FD
         let data = {
             user: FD.get('user'),
             pass: FD.get('pass')
         }
 
+        // Makes the fetch request to server
         fetch('/login', {
             method: 'POST',
             headers: { 'Content-type': 'application/json' },
