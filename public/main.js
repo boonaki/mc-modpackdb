b8007d14600958bdf20ded19188da1f12c10e8af
boonaki <joshdurr17@gmail.com>
2022-06-21 12:57:55 -0400

    deleted duplicate code

--- public/main.js
+++ public/main.js
@@ -1,26 +1,4 @@
-const login = document.querySelector('login')
 const signup = document.querySelector('#signup')
-
-login.addEventListener('submit', (event) => {
-    event.preventDefault()
-
-    let data = {
-        user: 'admin',
-        pass: 'password'
-    }
-
-    console.log(data)
-    
-    fetch('/login', {
-        method: 'POST',
-        headers: {'Content-type': 'application/json'},
-        body: JSON.stringify(data)
-    })
-        .then(res => res.json())
-        .then((res) => {
-            console.log(res)
-        })
-})
 
 signup.addEventListener('click', _ => {
     let data = {
