1fe8ac5bfa5be7dd1f9ec2ccc3e0d45f61026d5f
Raymond Guo <45112078+RayGuo357@users.noreply.github.com>
2022-06-21 10:11:04 -0400

    skeleton for sign up

--- main.js
+++ main.js
@@ -17,3 +17,20 @@ login.addEventListener('click', _ => {
             console.log(res)
         })
 })
+
+signup.addEventListener('click', _ => {
+    let data = {
+        user: 'admin',
+        pass: 'password'
+    }
+    
+    fetch('/signup', {
+        method: 'POST',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(data)
+    })
+        .then(res => res.json())
+        .then((res) => {
+            console.log(res)
+        })
+})
